490d528bedebab3b1b4c71dec50047598b560c2d
ZosL98 <Davidkof72@gmail.com>
2026-02-05 03:23:19 +0100

    Update Comment_management.php

--- demo/src/Models/Comment_management.php
+++ demo/src/Models/Comment_management.php
@@ -6,19 +6,17 @@
 
     class Comment_management extends Database
     {
-        private static function query_start()
-        {
-            return "SELECT comments.*, users.username FROM comments JOIN users ON users.id = comments.user_id";
-        }
+        private const query_start = "SELECT comments.*, users.username FROM comments JOIN users ON users.id = comments.user_id";
+        private const query_end = " ORDER BY comments.id ASC";
 
         public static function all()
         {
-            return parent::query(self::query_start() . " ORDER BY comments.id ASC")->fetchAll(\PDO::FETCH_ASSOC);
+            return parent::query(self::query_start . self::query_end)->fetchAll(\PDO::FETCH_ASSOC);
         }
 
         public static function search($username, $date)
         {
-            $query = self::query_start();
+            $query = self::query_start;
 
             $conditions = [];
             $params = [];
@@ -34,15 +32,15 @@
             }
 
             if (!empty($username) && !empty($date)) {
-                $query .= " WHERE " . $conditions[0] . " AND " . $conditions[1] . " ORDER BY comments.id ASC";
+                $query .= " WHERE " . $conditions[0] . " AND " . $conditions[1] . self::query_end;
                 return parent::query($query, $params)->fetchAll(\PDO::FETCH_ASSOC);
 
             } else if(!empty($username) && empty($date)) {
-                $query .= " WHERE " . $conditions[0] . " ORDER BY comments.id ASC";
+                $query .= " WHERE " . $conditions[0] . self::query_end;
                 return parent::query($query, $params)->fetchAll(\PDO::FETCH_ASSOC);
 
             } else if(empty($username) && !empty($date)) {
-                $query .= " WHERE " . $conditions[0] . " ORDER BY comments.id ASC";
+                $query .= " WHERE " . $conditions[0] . self::query_end;
                 return parent::query($query, $params)->fetchAll(\PDO::FETCH_ASSOC);
             }
         }
