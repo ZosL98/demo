04cf433ca4ed2dcf07a610a501d293f69169be94
ZosL98 <Davidkof72@gmail.com>
2026-02-13 02:31:58 +0100

    Update Database.php

--- demo/src/Core/Database.php
+++ demo/src/Core/Database.php
@@ -23,4 +23,24 @@ class Database {
 
         return $statement;
     }
+
+    public static function insert($table, $data = [])
+    {
+        $headers = [];
+
+        $keys = implode(",", array_keys($data));
+
+        foreach(explode(",", $keys) as $key) {
+            $key = substr($key, 1);
+
+            $headers[] = $key;
+        }
+
+        $headers = implode(",", $headers);
+        $values = implode(",", array_keys($data));
+
+        $query = "INSERT INTO $table ($headers) VALUES($values)";
+
+        Database::query($query, $data);
+    }
 }
