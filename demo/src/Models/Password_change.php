d6023022a2b3943ae844f62d908d804f42508813
ZosL98 <Davidkof72@gmail.com>
2026-02-13 02:25:36 +0100

    Update Password_change.php

--- demo/src/Models/Password_change.php
+++ demo/src/Models/Password_change.php
@@ -6,11 +6,6 @@
 
     class Password_change extends Database
     {
-        public static function check($id)
-        {
-            return parent::query("SELECT password FROM users WHERE id = :id", [":id" => $id])->fetch(\PDO::FETCH_ASSOC);
-        }
-
         public static function update($password, $id)
         {
             parent::query("UPDATE users SET password = :password WHERE id = :id", [":password" => $password, ":id" => $id]);
