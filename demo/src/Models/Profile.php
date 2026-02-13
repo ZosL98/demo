f7eeb1f235326407c4d76c1e6537131b46ec309f
ZosL98 <Davidkof72@gmail.com>
2026-02-13 04:28:02 +0100

    Update Profile.php

--- demo/src/Models/Profile.php
+++ demo/src/Models/Profile.php
@@ -6,11 +6,6 @@
 
     class Profile extends Database
     {
-        public static function find($header, $val)
-        {
-            return Database::query("SELECT * FROM users WHERE $header = :$header", ["$header" => $val])->fetch(\PDO::FETCH_ASSOC);
-        }
-
         public static function change($header, $val, $id)
         {
             Database::query("UPDATE users SET $header = :$header WHERE id = :id", [":$header" => $val, ":id" => $id]);
