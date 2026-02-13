26f51304effe57019f6636914eaa1a66ee100dac
ZosL98 <Davidkof72@gmail.com>
2026-02-13 02:32:51 +0100

    Update Auth.php

--- demo/src/Models/Auth.php
+++ demo/src/Models/Auth.php
@@ -3,18 +3,17 @@
     namespace App\Models;
 
     use App\Core\Database;
-    use App\Core\Functions;
 
     class Auth extends Database
     {
-        public static function check($header, $value)
+        public static function find($header, $value)
         {
             return parent::query("SELECT * FROM users WHERE $header = :$header", [":$header" => $value])->fetch(\PDO::FETCH_ASSOC);
         }
 
         public static function store($username, $email, $password)
         {
-            Functions::insert("users", [
+            Database::insert("users", [
                 ":username" => $username,
                 ":email" => $email,
                 ":password" => $password,
