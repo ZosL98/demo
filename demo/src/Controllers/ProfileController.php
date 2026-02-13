676421e7899538e49721a9c7fedfad2b48ad971c
ZosL98 <Davidkof72@gmail.com>
2026-02-13 02:27:39 +0100

    Update ProfileController.php

--- demo/src/Controllers/ProfileController.php
+++ demo/src/Controllers/ProfileController.php
@@ -21,59 +21,13 @@
 
         public function store()
         {
-            $errors = [];
+           $errors = Validator::validate([
+                'file' => ['image'],
+                'username' => ['min:3', 'max:20', 'unique:users,username'],
+                'email' => ['email', 'unique:users,email'],
+                'password' => ['password', 'min:3', 'required'],
+            ]);
 
-            $userData = Profile::find("id", Session::get('user_id'));
-
-            // file
-            if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
-                $file = $_FILES['file'];
-
-                if ($file['size'] > 5000000) {
-                    $errors['file']['size'] = 'File is too big';
-                }
-
-                $allowed = ['image/jpg', 'image/jpeg', 'image/png'];
-                if (!in_array($file['type'], $allowed)) {
-                    $errors['file']['type'] = 'File must be type jpg or jpeg or png';
-                }
-            }
-
-            // username
-            if (!empty(Request::input('username'))) {
-                $username = Request::input('username');
-                $res = Profile::find('username', $username);
-
-                if (!Validator::string($username, 3, 20)) {
-                    $errors['username'] = 'Username must be between 3 and 20 characters';
-                }
-
-                if ($res) {
-                    $errors['username'] = 'This username is already used';
-                }
-            }
-
-            // email
-            if (!empty(Request::input('email'))) {
-                $email = Request::input('email');
-                $res = Profile::find('email', $email);
-
-                if (!Validator::email($email)) {
-                    $errors['email'] = 'Your email is not well formed';
-                }
-
-                if ($res) {
-                    $errors['email'] = 'This email is already used';
-                }
-            }
-
-            // password
-            if (empty(Request::input('password'))) {
-                $errors['password'] = 'Password field is required';
-
-            } else if (!password_verify(Request::input('password'), $userData['password'])) {
-                $errors['password'] = 'Wrong password';
-            }
 
             if (!empty($errors)) {
                 Session::flash('errors', $errors);
@@ -83,6 +37,7 @@
 
             // changes
             if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
+                $file = $_FILES['file'];
                 $target_dir = __DIR__ . '/../../public/assets/uploads/';
                 $target_file = $target_dir . $file['name'];
 
@@ -103,6 +58,5 @@
             Session::flash('success', 'You have successfully updated your profile');
 
             redirect('profile');
-
         }
     }
